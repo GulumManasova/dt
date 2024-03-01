8029802cd77e2b882a83df7f6369bd4011c18689
Manasova Gulum <150650784+GulumManasova@users.noreply.github.com>
2024-03-01 11:31:11 +0600

    Add files via upload

---
+++ data.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Handling Missing Data Questions:
+# 
+# How do you identify and handle missing values in a Pandas DataFrame?
+# What is imputation, and why might it be useful in dealing with missing data?
+# 
+
+# Checking for missing values using isnull() and notnull()
+# In order to check missing values in Pandas DataFrame, we use a function isnull() and notnull(). Both function help in checking whether a value is NaN or not. These function can also be used in Pandas Series in order to find null values in a series.
+
+# In[ ]:
+
+
+
+
